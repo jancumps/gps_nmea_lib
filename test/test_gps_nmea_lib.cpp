680c142de6ae0aba943d7322703ced1224a4d3a8
Jan Cumps <jcumps@gmail.com>
2025-12-04 19:15:15 +0100

    use full parameter binding

--- test/test_gps_nmea_lib.cpp
+++ test/test_gps_nmea_lib.cpp
@@ -93,17 +93,10 @@ class nmea_gll : public nmeaTest<nmea::gll> {
 protected:
     nmea_gll() : parse_ok(false) {}
     void SetUp() override {
-#if __cpp_structured_bindings >= 202403L
-        if (auto [result] = o.from_data("$GPGLL,5051.83778,N,00422.55809,S,185427.150,A,N*4F")) {
+        if (auto [result, success] = o.from_data("$GPGLL,5051.83778,N,00422.55809,S,185427.150,A,N*4F")) {
             parse_ok = true;
             o = result;
         }
-#else
-        if (auto result = o.from_data("$GPGLL,5051.83778,N,00422.55809,S,185427.150,A,N*4F")) {
-            parse_ok = true;
-            o = result.result;
-        }
-#endif
     }
     bool parse_ok;
 };
